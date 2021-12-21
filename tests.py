a741964079f8510885a173f7be3394899aeebc06
Pietro Bovini <pietro.bovini@studio.unibo.it>
2021-12-21 18:35:29 +0100

    added tests to check the correctness of the calculation of energy and magnetization in the case of a fully polarized lattice

--- tests.py
+++ tests.py
@@ -5,55 +5,52 @@ Created on Fri Dec 17 14:59:01 2021
 @author: pietr
 """
 
-import functions_ising
+import functions_ising as fi
 import numpy as np
 
 import pytest
 
-#Test for functions_ising.initialize_state function
+#Test the lattice initialization function
 def test_lattice_dimensions(N = 3, M = 2):
-    lattice = functions_ising.initialize_state(N, M)
+    lattice = fi.initialize_state(N, M)
     assert len(lattice) == 3
     assert len(lattice[0]) == 2
     
-    
 def test_lattice_dimensions_polarized(N = 3, M = 2, choice = True, spin_up_pol = 0.3):
-    lattice = functions_ising.initialize_state(N, M)
+    lattice = fi.initialize_state(N, M)
     assert len(lattice) == 3
     assert len(lattice[0]) == 2
     
 def test_spin_values(N = 3, M = 2):
-    lattice = functions_ising.initialize_state(N, M)
+    lattice = fi.initialize_state(N, M)
     abs_spin = np.abs(lattice)
     assert abs_spin.all() == 1
     
 def test_spin_values_polarized(N = 3, M = 2, choice = True, spin_up_pol = 0.3):
-    lattice = functions_ising.initialize_state(N, M)
+    lattice = fi.initialize_state(N, M)
     abs_spin = np.abs(lattice)
     assert abs_spin.all() == 1
 
 def test_raises_error_lattice_dimensions(N = -1, M = -2):
     with pytest.raises(ValueError):
-        lattice = functions_ising.initialize_state(N, M)
+        lattice = fi.initialize_state(N, M)
 
 def test_choice_not_boolean(N = 2, M = 3, choice = 'not a boolean'):
     with pytest.raises(TypeError):
-        lattice = functions_ising.initialize_state(N, M, choice) 
+        lattice = fi.initialize_state(N, M, choice) 
 
 def test_spin_up_polarization_not_a_percentage(N = 2, M = 3, choice = True, spin_up_pol = 1.2):
     with pytest.raises(ValueError):
-        lattice = functions_ising.initialize_state(N, M, choice, spin_up_pol)
-        
+        lattice = fi.initialize_state(N, M, choice, spin_up_pol)        
 
-
-
-
-
-
-
-
-
-
+#Test functions for energy and magnetization calculation
+def test_correct_total_energy(N = 2, M = 3, choice = True, spin_up_pol = 1):
+    lattice = fi.initialize_state(N, M, choice, spin_up_pol)
+    assert fi.calculate_energy(lattice) == -N*M*4/2  
+    
+def test_correct_total_magnetization(N = 2, M = 3, choice = True, spin_up_pol = 1):
+    lattice = fi.initialize_state(N, M, choice, spin_up_pol)
+    assert fi.calculate_magnetization(lattice) == N*M
 
 
 
