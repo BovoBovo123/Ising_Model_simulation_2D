c2b3c525e4f30f5fc39792067a05caa20028e125
Pietro <pietrobovini@gmail.com>
2021-12-30 12:27:40 +0100

    Written all docstrings

--- plots_ising.py
+++ plots_ising.py
@@ -13,6 +13,38 @@ import logging
 
 
 def plots_T(T, energy, magnetization, saving = True, save_path = 'temperature_plot.png', load = False, load_path = ('ene_temp_path', 'mag_temp_path')):
+    """
+    This function plots energy and magnetization vs temperature, with data that is
+    either given or loaded, and can save it 
+
+    Parameters
+    ----------
+    T : 1D-like array
+        temperature points.
+    energy : 1D-like array
+        energy points.
+    magnetization : 1D-like array
+        magnetization points.
+    saving : bool, optional
+        if True, the plot is saved. The default is True.
+    save_path : string, optional
+        path to the save file. The default is 'temperature_plot.png'.
+    load : bool, optional
+        if True, data is loaded. The default is False.
+    load_path : 1D-like array, optional
+        list of strings of two files from which to load data; the first should be
+        for the energy. The default is ('ene_temp_path', 'mag_temp_path').
+
+    Returns
+    -------
+        None.
+    
+    Raises
+    ------
+        TypeError if the load switch is not a boolean
+
+    """
+    
     #Data either given or loaded
     if load == True:
         if len(energy)*len(magnetization)!= 0:
@@ -47,6 +79,43 @@ def plots_T(T, energy, magnetization, saving = True, save_path = 'temperature_pl
     
     
 def plots_steps(x_step, y_ene, y_mag, n_show, numb_T, saving = True, save_path = 'steps_plot.png', load = False, load_path = ('ene_steps_path', 'mag_steps_path')):
+    """
+    This function plots energy and magnetization vs temperature, with data that is
+    either given or loaded, and can save it 
+
+    Parameters
+    ----------
+    x_step : 1D-like array
+        number of step points.
+    y_ene : 1D-like array
+        energy points.
+    y_mag : 1D-like array
+        magnetization points.
+    n_show : int
+        index of temperature for which plots are shown.
+    numb_T : int
+        number of temperature points simulated.
+    saving : bool, optional
+        if True, the plot is saved. The default is True.
+    save_path : string, optional
+        path to save file. The default is 'steps_plot.png'.
+    load : bool, optional
+        if True, data is loaded. The default is False.
+    load_path : 1D-like array, optional
+        list of strings of two files from which to load data; the first should be
+        for the energy. The default is ('ene_steps_path', 'mag_steps_path').
+
+    Returns
+    -------
+        None.
+    
+    Raises
+    ------
+        TypeError if the load switch is not a boolean
+        ValueError if the temperature index is out of range
+
+    """
+    
     #Data either given or loaded
     if load == True:
         if len(y_ene)*len(y_mag)!= 0:
@@ -69,13 +138,13 @@ def plots_steps(x_step, y_ene, y_mag, n_show, numb_T, saving = True, save_path =
 
     #Energy plot vs steps
     sub_f =  f.add_subplot(1, 2, 1);
-    plt.scatter(x_step, y_ene, s = 20, marker = 'o', color = 'IndianRed')
+    plt.scatter(x_step, y_ene, s = 50, marker = 'o', color = 'IndianRed')
     plt.xlabel("Steps", fontsize=22)
     plt.ylabel("Energy ", fontsize=22)       
 
     #Magnetization plot vs steps
     sub_f =  f.add_subplot(1, 2, 2);
-    plt.scatter(x_step, y_mag, s = 20, marker = 'o', color = 'RoyalBlue')
+    plt.scatter(x_step, y_mag, s = 50, marker = 'o', color = 'RoyalBlue')
     plt.xlabel("Steps", fontsize=22)
     plt.ylabel("Magnetization ", fontsize=22) 
     
@@ -85,6 +154,32 @@ def plots_steps(x_step, y_ene, y_mag, n_show, numb_T, saving = True, save_path =
 
 
 def plot_evolution(evolution_states, N, M, times = (5, 10, 50, 100, 1000), saving = True, save_path = 'evolution_plot.png'):
+    """
+    This function plots the initial state of the lattice as well as its evolution
+    at five time instants.
+
+    Parameters
+    ----------
+    evolution_states : TYPE
+        DESCRIPTION.
+    N : int
+        length of the lattice.
+    M : int
+        width of the lattice.
+    times : 1D-like array, optional
+        five time instants when to store the evolved lattice spin configuration. 
+        The default is (5, 10, 50, 100, 1000).
+    saving : bool, optional
+        if True, the plot is saved. The default is True.
+    save_path : string, optional
+        path to save file. The default is 'evolution_plot.png'.
+
+    Returns
+    -------
+        None.
+
+    """
+    
     f = plt.figure(figsize=(30, 18), dpi=80)
     
     #Create a meshgrid; special case for N = M = 1
