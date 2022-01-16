a7eb87d839c30341f5275ffb5a4d4d5a61c9494f
Pietro <pietrobovini@gmail.com>
2022-01-16 17:00:58 +0100

    removed redundant tests, added new one to check evolution at beta values of 0 and infinity, and to check that the the same lattice is obtained independently of the intermediate steps

--- tests.py
+++ tests.py
@@ -30,8 +30,10 @@ def test_spins_all_up(N = 3, M = 2, spin_up_pol = 1):
     
 def test_spins_all_down(N = 3, M = 2, spin_up_pol = 0):
     lattice = fi.initialize_state(N, M, spin_up_pol)
-    for i in range(N):
-        for j in range(M):
+    length = len(lattice)
+    width = len(lattice[0])
+    for i in range(length):
+        for j in range(width):
             assert lattice[i][j] == -1    
 
 def test_raises_error_lattice_dimensions(N = -1, M = -2):
@@ -39,52 +41,54 @@ def test_raises_error_lattice_dimensions(N = -1, M = -2):
         lattice = fi.initialize_state(N, M)
 
 #Test the function that updates the lattice
-def test_metropolis_move(N = 2, M = 3, numb_T = 50, T_init = 1, T_final = 3):
-    #Test that dimensions, spins, energy and magnetization are as expected 
+def test_evolution_shape(N = 2, M = 3, beta = 1.0):
     lattice = fi.initialize_state(N, M)
-    T = np.linspace(T_init, T_final, numb_T)
-    for temp in range(numb_T):
-        beta = 1.0/T[temp]
-        lattice = fi.metropolis_move(lattice, beta)
-        abs_spin = np.abs(lattice)
-        assert abs_spin.all() == 1 
-        length = len(lattice)
-        width = len(lattice[0])
-        assert length == N
-        assert width == M
-        for x in range(length):
-            for y in range(width):
-                site_spin = lattice[x, y]
-                assert abs(site_spin) == 1
-                neighbour_spin = lattice[(x+1)%length, y] + lattice[x, (y+1)%width] + lattice[(x-1)%length, y] + lattice[x, (y-1)%width]
-                assert -4 <= neighbour_spin <= 4
-                energy_change = 2*site_spin*neighbour_spin
-                assert -8 <= energy_change <= 8
+    lattice = fi.metropolis_move(lattice, beta)
+    length = len(lattice)
+    width = len(lattice[0])
+    assert length == N
+    assert width == M
+
+def test_evolution_spins(N = 2, M = 3, beta = 1.0):
+    lattice = fi.initialize_state(N, M)
+    lattice = fi.metropolis_move(lattice, beta)
+    abs_spin = np.abs(lattice)
+    assert abs_spin.all() == 1 
+    
+def test_evolution_low_T(N = 2, M = 3, spin_up_pol = 1, beta = np.inf):
+    lattice = fi.initialize_state(N, M, spin_up_pol)
+    lattice = fi.metropolis_move(lattice, beta)
+    length = len(lattice)
+    width = len(lattice[0])
+    for i in range(length):
+        for j in range(width):
+            assert lattice[i][j] == 1    
+    
+def test_evolution_high_T(N = 2, M = 3, spin_up_pol = 1, seed = 1, beta = 0.0):
+    lattice = fi.initialize_state(N, M, spin_up_pol, seed)
+    evolved_lattice = fi.metropolis_move(lattice, beta)
+    simulated_lattice = ([1, -1, -1], [1, -1, -1])
+    assert np.array_equal(evolved_lattice, simulated_lattice) == True
 
 #Test the function that reads the configuration parameters
 def test_read_configuration(filename = ''):
     with pytest.raises(FileNotFoundError):
         config = fi.read_configuration(filename)
 
-def test_simulate(N = 2, M = 3, beta = 1):
-    #Test that dimensions, spins, energy and magnetization are as expected 
+#Test the function that simulated lattice evolution at certain time instants
+def test_simulate_length(N = 2, M = 3, beta = 1.0, times = [1, 2, 3, 4, 5]):
     lattice = fi.initialize_state(N, M)
-    evolved_states = fi.simulate(lattice, beta)
-    for state in evolved_states:
-        abs_spin = np.abs(state)
-        assert abs_spin.all() == 1 
-        length = len(state)
-        width = len(state[0])
-        assert length == N
-        assert width == M
-        for x in range(length):
-            for y in range(width):
-               site_spin = lattice[x, y]
-               assert abs(site_spin) == 1
-               neighbour_spin = lattice[(x+1)%length, y] + lattice[x, (y+1)%width] + lattice[(x-1)%length, y] + lattice[x, (y-1)%width]
-               assert -4 <= neighbour_spin <= 4
-               energy_change = 2*site_spin*neighbour_spin
-               assert -8 <= energy_change <= 8
+    evolved_states = fi.simulate(lattice, beta, times)
+    assert len(evolved_states) == 6
+    
+def test_simulate_time_independent(N = 2, M = 3, seed = 1, beta = 1.0, times1 = [2, 4, 6, 8, 10], times2 = [2, 5, 7, 9, 10]):    
+    lattice1 = fi.initialize_state(N, M, seed = seed)
+    evolved_states1 = fi.simulate(lattice1, beta, times1)
+    lattice2 = fi.initialize_state(N, M, seed = seed)
+    evolved_states2 = fi.simulate(lattice2, beta, times2)
+    assert np.array_equal(evolved_states1[1], evolved_states2[1]) == True
+    assert np.array_equal(evolved_states1[5], evolved_states2[5]) == True
+
 
 
 
