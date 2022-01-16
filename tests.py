d6ac981633ed13c2c3e0a96290f81ed39fd303a1
Pietro <pietrobovini@gmail.com>
2022-01-16 19:08:05 +0100

    Added tests for energy and magnetization; added all docstrings

--- tests.py
+++ tests.py
@@ -13,22 +13,48 @@ import pytest
 
 #Test the lattice initialization function
 def test_lattice_dimensions(N = 3, M = 2):
+    """
+    Test that the shape of the lattice is the one expected.
+
+    """
+    
     lattice = fi.initialize_state(N, M)
     assert len(lattice) == 3
     assert len(lattice[0]) == 2
     
+    
 def test_spin_values(N = 3, M = 2):
+    """
+    Test that all spins are either +1 or -1; 
+    this will also fix minimum and maximum energy and magnetization.
+
+    """
+    
     lattice = fi.initialize_state(N, M)
     abs_spin = np.abs(lattice)
     assert abs_spin.all() == 1
     
+    
 def test_spins_all_up(N = 3, M = 2, spin_up_pol = 1):
+    """
+    Test that the lattice can be generated with all spins up.
+
+    """
+    
     lattice = fi.initialize_state(N, M, spin_up_pol)
-    for i in range(N):
-        for j in range(M):
+    length = len(lattice)
+    width = len(lattice[0])
+    for i in range(length):
+        for j in range(width):
             assert lattice[i][j] == 1
     
+    
 def test_spins_all_down(N = 3, M = 2, spin_up_pol = 0):
+    """
+    Test that the lattice can be generated with all spins down.
+
+    """
+    
     lattice = fi.initialize_state(N, M, spin_up_pol)
     length = len(lattice)
     width = len(lattice[0])
@@ -36,12 +62,24 @@ def test_spins_all_down(N = 3, M = 2, spin_up_pol = 0):
         for j in range(width):
             assert lattice[i][j] == -1    
 
+
 def test_raises_error_lattice_dimensions(N = -1, M = -2):
+    """
+    Test that an error is raised if the lattice dimensions are negative.
+
+    """
+    
     with pytest.raises(ValueError):
         lattice = fi.initialize_state(N, M)
 
+
 #Test the function that updates the lattice
 def test_evolution_shape(N = 2, M = 3, beta = 1.0):
+    """
+    Test that the lattice maintains the correct shape when evolved.
+
+    """
+    
     lattice = fi.initialize_state(N, M)
     lattice = fi.metropolis_move(lattice, beta)
     length = len(lattice)
@@ -49,13 +87,27 @@ def test_evolution_shape(N = 2, M = 3, beta = 1.0):
     assert length == N
     assert width == M
 
+
 def test_evolution_spins(N = 2, M = 3, beta = 1.0):
+    """
+    Test that the lattice spins remain either +1 or -1 when evolved.
+
+    """
+    
     lattice = fi.initialize_state(N, M)
     lattice = fi.metropolis_move(lattice, beta)
     abs_spin = np.abs(lattice)
     assert abs_spin.all() == 1 
     
+    
 def test_evolution_low_T(N = 2, M = 3, spin_up_pol = 1, beta = np.inf):
+    """
+    Test that at zero temperature a fully polarized lattice does not change;
+    this is because there are no thermal fluctuations to provide the energy to move
+    from the energy minimum.
+
+    """
+    
     lattice = fi.initialize_state(N, M, spin_up_pol)
     lattice = fi.metropolis_move(lattice, beta)
     length = len(lattice)
@@ -64,28 +116,77 @@ def test_evolution_low_T(N = 2, M = 3, spin_up_pol = 1, beta = np.inf):
         for j in range(width):
             assert lattice[i][j] == 1    
     
+    
 def test_evolution_high_T(N = 2, M = 3, spin_up_pol = 1, seed = 1, beta = 0.0):
+    """
+    Test that a lattice evolves as simulated; in particular, that a fully
+    polarized lattice at infinite temperature gets disordered easily.
+
+    """
+    
     lattice = fi.initialize_state(N, M, spin_up_pol, seed)
     evolved_lattice = fi.metropolis_move(lattice, beta)
     simulated_lattice = ([1, -1, -1], [1, -1, -1])
     assert np.array_equal(evolved_lattice, simulated_lattice) == True
 
+
+#Test the functions that calculate energy and magnetization
+def test_energy(N = 2, M = 3, seed = 1):
+    """
+    Test that the energy is calculated correctly.
+
+    """
+    
+    lattice = fi.initialize_state(N, M, seed)
+    energy = fi.calculate_energy(lattice)
+    assert energy == 4.0
+
+
+def test_mag(N = 2, M = 3, seed = 1):
+    """
+    Test that the magnetization is calculated correctly.
+
+    """
+    
+    lattice = fi.initialize_state(N, M, seed)
+    mag = fi.calculate_magnetization(lattice)
+    assert mag == 2.0
+    
+
 #Test the function that reads the configuration parameters
 def test_read_configuration(filename = ''):
+    """
+    Test that an error is raised if the configuration file is not found.
+
+    """
+    
     with pytest.raises(FileNotFoundError):
         config = fi.read_configuration(filename)
 
-#Test the function that simulated lattice evolution at certain time instants
+
+#Test the function that simulates lattice evolution at certain time instants
 def test_simulate_length(N = 2, M = 3, beta = 1.0, times = [1, 2, 3, 4, 5]):
+    """
+    Test that the length of the array containing the lattice at various time instants 
+    is one longer (due to the initial state added).
+
+    """
+    
     lattice = fi.initialize_state(N, M)
     evolved_states = fi.simulate(lattice, beta, times)
-    assert len(evolved_states) == 6
+    assert len(evolved_states) == len(times) + 1
     
-def test_simulate_time_independent(N = 2, M = 3, seed = 1, beta = 1.0, times1 = [2, 4, 6, 8, 10], times2 = [2, 5, 7, 9, 10]):    
-    lattice1 = fi.initialize_state(N, M, seed = seed)
-    evolved_states1 = fi.simulate(lattice1, beta, times1)
-    lattice2 = fi.initialize_state(N, M, seed = seed)
-    evolved_states2 = fi.simulate(lattice2, beta, times2)
+    
+def test_simulate_times_independent(N = 2, M = 3, seed = 1, beta = 1.0, times1 = [2, 4, 6, 8, 10], times2 = [2, 5, 7, 9, 10]):    
+    """
+    Test that from the same initial state, the same evolved state is obtained at the same 
+    time insant independently from the intermediate steps.
+
+    """
+    
+    lattice = fi.initialize_state(N, M, seed = seed)
+    evolved_states1 = fi.simulate(lattice, beta, times1)
+    evolved_states2 = fi.simulate(lattice, beta, times2)
     assert np.array_equal(evolved_states1[1], evolved_states2[1]) == True
     assert np.array_equal(evolved_states1[5], evolved_states2[5]) == True
 
